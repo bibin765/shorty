04239a270b1d82df71db7224452bc342d6d14fbd
BIBIN OOMMEN MATHEW <bibin765@gmail.com>
2020-03-06 19:40:35 +0530

    veutify

---
+++ client/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      dark
+      hide-on-scroll
+    >
+      <div class="d-flex align-center">
+
+        <v-img
+          alt="Shorty logo"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="120"
+          src="./assets/drawing.svg"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+    <br /><br /><br /><br />
+  <v-container
+    align="center"
+    justify="center">
+    <v-row>
+      <v-col>
+        <story-view />
+      </v-col>
+      <v-col>
+        <story-view />
+      </v-col>
+      <v-col>
+        <story-view />
+      </v-col>
+      <v-col>
+        <story-view />
+      </v-col>    
+    </v-row>
+    <v-row>
+        <v-col>
+          <story-view />
+        </v-col> 
+        <v-col>
+          <story-view />
+        </v-col> 
+        <v-col>
+          <story-view />
+        </v-col> 
+        <v-col>
+          <story-view />
+        </v-col> 
+    </v-row>
+  </v-container>
+</v-app>
+</template>
+
+<script>
+import StoryView from './components/StoryView';
+
+export default {
+  name: 'App',
+
+  components: {
+    StoryView,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/StoryView.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+  <v-card-title>
+    Story Title
+  </v-card-title>
+    <v-card-text>
+      <div class="text--primary">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu ligula semper, ultrices enim non, porta felis. Maecenas sem ex, pellentesque consectetur libero nec, tristique ornare massa. Nunc eget commodo elit, ut dapibus elit. Integer lorem mauris, tristique a dapibus vel, condimentum sit amet metus. Nullam sed enim id diam vulputate maxim
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'StoryView',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
